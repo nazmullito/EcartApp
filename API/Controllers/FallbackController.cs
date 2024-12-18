f5e0dc6a4ced2eb3c83172a7a636602aa5bdd380
Nazmul Islam <nazmul.islam@adndiginet.com>
2024-12-18 21:20:15 +0600

    .

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -4,10 +4,19 @@ namespace API.Controllers
 {
     public class FallbackController : Controller
     {
+        private readonly IWebHostEnvironment _env;
+
+        public FallbackController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         public IActionResult Index()
         {
-            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
-                "wwwroot\\browser", "index.html"), "text/HTML");
+            //return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+            //    "wwwroot\\browser", "index.html"), "text/HTML");
+
+            var indexHtmlPath = Path.Combine(_env.WebRootPath, "index.html");
+            return PhysicalFile(indexHtmlPath, "text/html");
         }
     }
 }
